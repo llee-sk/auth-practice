af6f2348fbb6c6d6e85bb8e7d344417b82e280e4
lleess <llee_ss@naver.com>
2026-04-28 15:54:39 +0900

    Initial commit

---
+++ src/main/java/com/example/auth_practice/AuthPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthPracticeApplication.class, args);
+	}
+
+}
